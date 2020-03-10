e16356764fef28685eaf4f0a0733f2c1f49f8355
Bidowaniec Stanislaw <Stanislaw.Bidowaniec@baslerhofmann.de>
2020-03-10 09:22:09 +0100

    comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # c-_autocad_helloworld
 c#, autocad, helloworld
+
+Repair paths in:
+
+myproject.csproj: AutoCAD
+compile.bat: .NET
--- acad_plug.cs
+++ acad_plug.cs
@@ -1,23 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+// import AcMgd, AcDbMgd, AcCoreMgd referenced in .csproj
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
 
-
 namespace provaAcNetDll
 {
-public class Classe
-{
-[CommandMethod("HelloWorld")]
-public void HelloWorldCommand()
-{
-Editor ed =
-Application.DocumentManager.MdiActiveDocument.Editor;
-ed.WriteMessage("Hello Basler&Hofmann");
-}
-
-}
+    public class Classe
+    {
+        [CommandMethod("HelloWorld")]
+        public void HelloWorldCommand()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("Hello Przemo :p");
+        }
+    }
 }
